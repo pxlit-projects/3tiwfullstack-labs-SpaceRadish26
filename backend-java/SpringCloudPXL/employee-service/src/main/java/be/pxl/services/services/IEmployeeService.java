3e67bd4e230a3415476aec1c7eb8f6a4c695252f
SpaceRadish26 <brecht.deweyer666@gmail.com>
2024-09-19 16:59:13 +0200

    vid af, switch nr desktop

---
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/services/IEmployeeService.java
@@ -0,0 +1,10 @@
+package be.pxl.services.services;
+
+import be.pxl.services.domain.Employee;
+
+import java.util.List;
+
+public interface IEmployeeService {
+
+    List<Employee> getAllEmployees();
+}
